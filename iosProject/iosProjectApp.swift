79330ae9314edb7cdb0b689640c65e1bae96c092
rmenjivar <menjivarmenjivar.rimm@gmail.com>
2024-04-17 18:52:13 -0700

    init

---
+++ iosProject/iosProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosProjectApp.swift
+//  iosProject
+//
+//  Created by Rogelio on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct iosProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
